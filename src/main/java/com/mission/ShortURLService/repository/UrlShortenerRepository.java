dc42bffdb7421f9dd242100790d72698b8e0b598
healtheefart <961210eee@naver.com>
2022-06-08 17:27:12 +0900

    chore : requestDto에서 @Validated 제거, repository에서 @Repository 제거

--- src/main/java/com/mission/ShortURLService/repository/UrlShortenerRepository.java
+++ src/main/java/com/mission/ShortURLService/repository/UrlShortenerRepository.java
@@ -3,9 +3,7 @@ package com.mission.ShortURLService.repository;
 import com.mission.ShortURLService.entity.Url;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UrlShortenerRepository extends JpaRepository<Url, Integer> {
 
 	Optional<Url> findByOriginUrl(String urlStr);
